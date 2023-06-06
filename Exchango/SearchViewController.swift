5da899bd917b8a1808ccc5f5e61fb03a81f4d6e1
Natasha Machado <natashamachado@Natashas-MacBook-Pro.local>
2023-06-05 19:48:14 -0700

    First concept and Design thru storyboard

---
+++ Exchango/SearchViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SearchViewController.swift
+//  Exchango
+//
+//  Created by Natasha Machado on 2023-06-05.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

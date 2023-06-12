91c3b5daf2fadb5033347cad54f3289cececcb8b
Natasha Machado <natashamachado@Natashas-MacBook-Pro.local>
2023-06-12 10:53:16 -0700

    constrains and views on sb

--- Exchango/ProfileViewController.swift
+++ Exchango/ProfileViewController.swift
@@ -9,7 +9,21 @@ import UIKit
 
 class ProfileViewController: UIViewController {
 
-    override func viewDidLoad() {
+  
+  @IBOutlet var ProfilePictureView: UIImageView!
+  @IBOutlet var UserNameLabel: UILabel!
+  @IBOutlet var MemberLabel: UILabel!
+  @IBOutlet var MessagesView: UIView!
+  @IBOutlet var WatchlistView: UIView!
+  @IBOutlet var SavedView: UIView!
+  @IBOutlet var RecentView: UIView!
+  @IBOutlet var WalletView: UIView!
+  @IBOutlet var HelpView: UIView!
+  @IBOutlet var SettingsView: UIView!
+  
+  
+  
+  override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
---
+++ Exchango/WelcomeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  WelcomeViewController.swift
+//  Exchango
+//
+//  Created by Natasha Machado on 2023-06-06.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

09b1dae6470d313485d80a1723def04e2412aaeb
Natasha Machado <natashamachado@Natashas-MacBook-Pro.local>
2023-06-20 10:28:14 -0700

    more screens and fuctions

---
+++ Exchango/SignUpViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SignUpViewController.swift
+//  Exchango
+//
+//  Created by Natasha Machado on 2023-06-20.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+  
+  @IBOutlet var FirstName: UITextField!
+  @IBOutlet var LastName: UITextField!
+  @IBOutlet var Username: UITextField!
+  @IBOutlet var Email: UITextField!
+  @IBOutlet var DateOfBirth: UIDatePicker!
+  @IBOutlet var Password: UITextField!
+  @IBOutlet var SignUpCompleteButton: UIButton!
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+   
+
+}
